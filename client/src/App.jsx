19619a72e6f06b3b40d5489c9605dd02fcbb602c
ignacio-001 <tsk7558@gmail.com>
2022-07-16 16:40:14 +0530

    Establisded connection in Room using console log

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import io from "socket.io-client";
+import { useState } from 'react';
+import Chat from './Chat';
+const socket = io.connect("http://localhost:3001");
+function App() {
+  const [username,setUsername]=useState("");
+  const [room,setRoom]=useState("");
+
+  const joinRoom = () =>{
+    // console.log("used");
+    if(username !== "" && room !== ""){
+      socket.emit("join_room",room);
+    }
+  };
+  return (
+    <div className="App">
+      <h3> Join a Chat</h3>
+      <input type="text" placeholder="john..." onChange={(event)=>{
+        setUsername(event.target.value);
+      }}/>
+      <input type="text " placeholder="Room..id..."onChange={(event)=>{
+        setRoom(event.target.value);
+      }}/>
+      <button onClick={joinRoom} >Join a room</button> 
+      <Chat socket={socket} username={username} room={room}/>
+       </div>
+  );
+}
+
+export default App;
---
+++ client/src/Chat.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+
+function Chat({socket,username,room}) {
+  const [currentMessage,setcurrentMessage]=useState("");
+  const sendMessage = async()=>{
+    if(currentMessage!==""){
+      const messageData={
+        room:room,
+        author:username,
+        message: currentMessage,
+        time: 
+        new Date(Date.now()).getHours()+
+        ":" + new Date(Date.now()).getMinutes(),
+      };
+      await socket.emit("send_message", messageData);
+    }
+  };
+  useEffect(() => {
+    socket.on("receive_message",(data)=>{
+      console.log(data);
+    });
+  }, [socket])
+  
+  return (
+    <div>
+        <div className="chat-header">
+          <p>live Chat</p>
+        </div>
+      <div className="chat-body">
+
+      </div>
+      <div className="chat-footer">
+        <input type="text" placeholder="Hey..."onChange={(event)=>{
+          setcurrentMessage(event.target.value);
+        }} />
+        <button onClick={sendMessage}>&#9658;</button>
+      </div>
+    </div>
+  )
+}
+
+export default Chat
